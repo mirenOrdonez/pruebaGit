b7013a276ad86b8bae3955ee835047c9904c8c75
Miren Ordóñez <miren.ordonez@gmail.com>
2020-01-14 19:19:37 +0100

    Commit inicial del proyecto.

---
+++ src/pruebagit/PruebaGit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagit;
+
+/**
+ *
+ * @author mirenordonezdearce
+ */
+public class PruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba GIT");
+        /*hay que clicar sobre el proyecto, y darle a versioning. Ahí iniciar git repository
+         * 
+         */
+    }
+    
+}
